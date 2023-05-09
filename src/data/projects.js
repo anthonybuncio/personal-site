90945c6266776241e3b62e25c015943342330bb2
anthonybuncio <anthonyb.ft@gmail.com>
2023-05-09 15:19:15 -0500

    Added more data...updated projects list

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,38 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Ready, Set, STEM',
+    title: 'Play-i AI Generated Playlist',
+    subtitle: 'AI Generated Playlist',
+    link: 'https://chatgpt-dj.vercel.app/',
+    image: '/images/projects/gptplaylist.jpg',
+    date: '2023-03-30',
+    desc:
+      'Elevate your music experience with our AI playlist generator that curates personalized playlists based on your preferred genre, year, mood, or activity. '
+      + 'Our advanced algorithms analyze your music preferences and create playlists that perfectly match your current mood or activity. '
+      + 'Whether youʼre working out, relaxing, or in need of a pick-me-up, our playlist generator has got you covered.',
+  },
+  {
+    title: 'StreetLensHouston - Cloud sharing dashcam video platform',
+    subtitle: 'Cloud sharing dashcam video platform',
+    link: 'https://dashcam-uploader.vercel.app/',
+    image: '/images/projects/dashcam.jpg',
+    date: '2023-01-30',
+    desc:
+      'Stay protected and connected with our Dashcam cloud sharing platform that allows you to share your footage and critical accident information with ease. '
+      + 'Our platform makes it simple to upload and share your dashcam footage and additional details about an accident, ensuring that you have a clear record of what happened. '
+      + 'With our platform, you can feel confident knowing that youʼre prepared in case of an accident.',
+  },
+  {
+    title: 'Ready, Set, STEM - 2022 NASA Hackathon',
     subtitle: '2022 NASA Hackathon Submission',
     link: 'https://jsc-hackathon.vercel.app/',
     image: '/images/projects/jsc.jpg',
     date: '2022-01-30',
     desc:
-      'Final submission for JSC NASA Hackathon 2022. Inspired to improve accessibility and inclusivity at NASA, '
-      + 'we created a three-step strategy that will increase the number of children entering into the STEM pipeline using subjective teaching methodologies.',
+      'Explore the wonders of space and ignite a passion for STEM with our educational app, '
+      + 'designed to teach children about NASA careers and aerospace engineering through engaging activities and challenges. '
+      + 'Inspired to improve accessibility and inclusivity at NASA, '
+      + 'our team created a three-step strategy that will increase the number of children entering into the STEM pipeline using subjective teaching methodologies.',
   },
   {
     title: 'Collab OS',
@@ -28,9 +52,8 @@ const data = [
     image: '/images/projects/fourthqtr.jpg',
     date: '2019-03-28',
     desc:
-      'An NBA scoreboard app for the MacOS menu bar, built with ElectronJS. '
-      + 'Upon using, scores are updated every 30 seconds and includes dark mode. '
-      + 'Future updates include adding mutiple sports.',
+      'Stay up-to-date on NBA scores in real-time with our Electron JS app that displays live scores in your macOS menu bar, keeping you informed without interrupting your workflow.'
+      + ' Scores are updated every 30 seconds and includes dark mode. Future updates include adding mutiple sports.',
   },
   {
     title: 'DevLink',
@@ -39,8 +62,9 @@ const data = [
     image: '/images/projects/devlink.jpg',
     date: '2019-02-15',
     desc:
-      'Facebook for devs. A social network for developers to engage with each other. '
-      + 'Includes user authentication and messaging.',
+      'A social network platform specifically designed for software engineers and web developers to connect, collaborate and share ideas with each other. '
+      + 'The platform features user authentication for security and allows users to send messages to one another. '
+      + 'This personalized network will provide a space where users can interact with like-minded individuals and exchange valuable knowledge and insights within their field of expertise.',
   },
 ];
 
